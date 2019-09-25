083102c353dd027f27b7960ca97f7da3535c5070
Neil Campbell <neildcampbell@hotmail.com>
2019-09-25 16:50:03 +0100

    added table to css

--- app.rb
+++ app.rb
@@ -10,15 +10,15 @@ current_dir = Dir.pwd
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
 
 class MakersBnb < Sinatra::Base
-enable :sessions
-register Sinatra::Flash
+  enable :sessions
+  register Sinatra::Flash
 
   get '/' do
     erb :index
   end
 
   post '/signup' do
-   bcrypt_password = BCrypt::Password.create("#{params['password']}")
+    bcrypt_password = BCrypt::Password.create("#{params['password']}")
 
     User.create(first_name: "#{params['first_name']}",
       last_name: "#{params['last_name']}",
@@ -26,40 +26,40 @@ register Sinatra::Flash
       password: bcrypt_password)
 
 
-    redirect('/login')
-  end
-
-  get '/login' do
-
-    erb :login
-  end
-
-  post '/login' do
-     @user = User.find_by(email: "#{params['email']}")
-    if BCrypt::Password.new(@user.password) == params['password']
-      redirect '/spaces'
-    else flash[:notice] = "Credentials Incorrect"
-      redirect '/login'
+      redirect('/login')
     end
-end
 
-get '/spaces' do
- @spaces = Space.all.order(created_at: :desc)
- erb :spaces
-end
+    get '/login' do
 
-post '/spaces' do
-   Space.create(name: "#{params['name']}",
-     description: "#{params['description']}",
-     price: "#{params['price_per_night']}",
-     available_from: "#{params['available_from']}",
-     available_to: "#{params['available_to']}")
-     @spaces = Space.all.order(created_at: :desc)
-     erb :spaces
-   end
-   get '/spaces/new' do
+      erb :login
+    end
+
+    post '/login' do
+      @user = User.find_by(email: "#{params['email']}")
+      if BCrypt::Password.new(@user.password) == params['password']
+        redirect '/spaces'
+      else flash[:notice] = "Credentials Incorrect"
+        redirect '/login'
+      end
+    end
+
+    get '/spaces' do
+      @spaces = Space.all.order(created_at: :desc)
+      erb :spaces
+    end
+
+    post '/spaces' do
+      Space.create(name: "#{params['name']}",
+        description: "#{params['description']}",
+        price: "#{params['price_per_night']}",
+        available_from: "#{params['available_from']}",
+        available_to: "#{params['available_to']}")
+        @spaces = Space.all.order(created_at: :desc)
+        erb :spaces
+      end
+      get '/spaces/new' do
         erb :spaces_new
       end
 
-run! if app_file == $0
-end
+      run! if app_file == $0
+    end
