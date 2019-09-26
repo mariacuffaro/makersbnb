d157595e0c7e5341fc9b965e8a2edc8f2cbd8347
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-09-26 14:54:13 +0100

    added a date filter

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/activerecord/rake'
 require './database_connection_setup.rb'
 require 'sinatra/flash'
 require 'bcrypt'
+require 'date'
 # require './lib/user.rb'
 
 current_dir = Dir.pwd
@@ -12,20 +13,17 @@ Dir["#{current_dir}/models/*.rb"].each { |file| require file }
 class MakersBnb < Sinatra::Base
   enable :sessions
   register Sinatra::Flash
-
+  
   get '/' do
     erb :index
   end
 
   post '/signup' do
     bcrypt_password = BCrypt::Password.create("#{params['password']}")
-
     User.create(first_name: "#{params['first_name']}",
       last_name: "#{params['last_name']}",
       email: "#{params['email']}",
       password: bcrypt_password)
-
-
       redirect('/login')
     end
 
@@ -49,18 +47,26 @@ class MakersBnb < Sinatra::Base
     end
 
     post '/spaces' do
-      Space.create(name: "#{params['name']}",
-        description: "#{params['description']}",
-        price: "#{params['price_per_night']}",
-        available_from: "#{params['available_from']}",
-        available_to: "#{params['available_to']}")
-        @spaces = Space.all.order(created_at: :desc)
-        erb :spaces
-      end
+      if params['start_date'] == nil
+        Space.create(name: "#{params['name']}",
+          description: "#{params['description']}",
+          price: "#{params['price_per_night']}",
+          available_from: "#{params['available_from']}",
+          available_to: "#{params['available_to']}")
+          @spaces = Space.all.order(created_at: :desc)
+        else
+          end_date = Date.parse(params[:end_date])
+          start_date = Date.parse(params[:start_date])
+          @spaces = Space.where("available_from <= :start_date  AND available_to >= :end_date",
+            :start_date => start_date,
+            :end_date => end_date)
+          end
+          erb :spaces
+        end
 
-      get '/spaces/new' do
-        erb :spaces_new
-      end
+        get '/spaces/new' do
+          erb :spaces_new
+        end
 
-      run! if app_file == $0
-    end
+        run! if app_file == $0
+      end
